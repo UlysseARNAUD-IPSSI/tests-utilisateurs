79dd5d803966cc90e537c0561e6512db9bde828d
Ulysse ARNAUD <mexanga@gmail.com>
2020-07-15 19:47:06 +0200

    Ajout des erreurs

---
+++ Errors/InvalidArgumentsError/index.js
@@ -0,0 +1,45 @@
+/**
+ * Erreur arguments invalides
+ * @class InvalidArgumentsError
+ * @extends Error
+ */
+class InvalidArgumentsError extends Error {
+
+    /**
+     * Singleton
+     * @returns {InvalidArgumentsError}
+     */
+    static singleton() {
+        return new this;
+    }
+
+    /**
+     * @constructor
+     * @param params
+     */
+    constructor(...params) {
+        super(...params);
+
+        if(Error.captureStackTrace) {
+            Error.captureStackTrace(this, InvalidArgumentsError);
+        }
+        /**
+         * Nom de la classe
+         * @type {string}
+         */
+        this.name = 'InvalidArgumentsError';
+
+        /**
+         * Message de l'erreur
+         * @type {string}
+         */
+        this.message = 'Arguments invalides.';
+
+        /**
+         * Date où l'erreur a été encourue
+         * @type {Date}
+         */
+        this.date = new Date();
+    }
+}
+module.exports = InvalidArgumentsError;
